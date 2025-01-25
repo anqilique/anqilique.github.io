586fd4cd2ca5d9ef73b840d5a416b9dc18893662
anqilique <107088774+anqilique@users.noreply.github.com>
2025-01-25 14:53:35 +1300

    Revamped Files

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
-const textOptions = [
-    "Made by a student in need of more sleep. 💤", 
-    "Looking for something? 👁👄👁",
-    "This website is a work in progress. 👷‍♀️",
-    "Pick your poison. 👀",
-    "Probably running on caffeine. ☕",
-    "Proof I can organise something. ✅",
-    "Click something, I dare you.",
-    "Click responsibly.",
-    "A collection of links you didn't ask for.",
-    "Made this instead of doing something productive.",
-    "Shh... The broken stuff is a ✨feature✨."
+let bot = new RiveScript();
+
+const message_container = document.querySelector('.messages');
+const form = document.querySelector('form');
+const input_box = document.querySelector('input');
+
+const brains = [
+    "./brains/dictionary.rive",
+    "./brains/help.rive",
+    "./brains/search.rive",
+    "./brains/about.rive",
+    "./brains/contact.rive",
+    "./brains/linktree.rive",  // Because 'links' is a reserved word in RiveScript
+    "./brains/status.rive",
+    "./brains/fun.rive",
+    "./brains/convo.rive",
 ];
 
-const kaomojis = [
-    "(＾▽＾)",
-    "(^_^)/",
-    "(^_^)",
-    "(*≧▽≦)",
-    "(^o^)/",
-    "（＾ｖ＾）",
-    "(・o・)",
-    "(O_O)",
-    "(°o°)",
-    "(@_@)",
-    "(・_・ヾ)",
-    "(*´ω｀*)",
-    "(❤ω❤)",
-    "( ˘ ³˘)♥",
-    "(づ￣ ³￣)づ",
-    "(^・ω・^ )",
-    "(=^･ω･^=)",
-    "（￣～￣;）"
-];
+const greeting = `Hi there! I am <span class="green">LeekBot</span>. 🥬
+<br><br>
+I've been created by <span class="green">Anqilique</span> to answer your questions. <br> I can fetch links, 
+search the web, and more! 😁
 
-function replaceText() {
-    const randomIndex = Math.floor(Math.random() * textOptions.length);
-    document.getElementById("random-text").textContent = textOptions[randomIndex];
+<br><br><br>
+Try typing in one of the following:<br>
+<span class="lightgreen">help / about / contact / links / status / fun / clear</span>
+`
+
+const no_brains_msg = `Uh oh! LeekBot's brains not found. 😱🧠
+<br>Please let Anqilique know or come back later!
+`
+
+const loading_brains_msg = "<sub>Loading brains... 🧠 <br><br>If nothing appears, please let Anqilique know or come back later! 😱</sub>"
+
+const brains_loaded_msg = "<sub>Brains loaded! 🧠 <br><br>---</sub>"
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    selfReply(input_box.value);
+    input_box.value = '';
+});
+
+
+botReply(loading_brains_msg);
+
+function botReply(message) {
+    message_container.innerHTML += `<div class="bot">${message}</div>`;
+    location.href = '#latest';
+
+    input_box.focus();
 }
 
-function changeKao() {
-	const randomNumber = Math.floor(Math.random() * kaomojis.length);
-	const randomKaomoji = kaomojis[randomNumber];
-	document.getElementById("kaomoji").innerText = randomKaomoji;
+function selfReply(message) {
+    message_container.innerHTML += `<div class="self">${message}</div>`;
+    location.href = '#latest';
+
+    bot.reply("local-user", message).then(function (reply) {
+        botReply(reply);
+    });
 }
 
-document.addEventListener("DOMContentLoaded", replaceText);
-document.addEventListener("DOMContentLoaded", changeKao);
+function botLoaded() {
+
+    if (window.innerWidth < 1000) {
+        window.location.replace("http://127.0.0.1:5500/links.html");
+    } else {
+        bot.loadFile(brains).then(botReady).catch(botNotReady);
+    }
+}
+
+function botReady() {
+    bot.sortReplies();
+    botReply(brains_loaded_msg);
+    botReply(greeting);
+
+    input_box.focus();
+}
+
+function botNotReady(err) {
+    console.log(no_brains_msg, err);
+}
