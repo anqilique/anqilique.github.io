07dc7fe6dfae4330fa3db11b4a420727c3dd29dd
anqilique <angelbhulot@gmail.com>
2026-07-12 19:08:01 +1200

    style/adjust sizes

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,15 @@ const images = [
 
 function App() {
   return (
-    <div className="mx-2 mb-5 sm:mx-5 md:mx-20 xl:mx-40 2xl:mx-80 h-screen">
+    <div className="mx-2 mb-5 sm:mx-5 md:mx-20 xl:mx-30 h-180 md:h-200">
       <h1 className="text-2xl text-center my-5">anqilique's desktop</h1>
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
-        <div className="col-span-1 xl:row-span-3 md:min-h-60">
+        <div className="col-span-1 xl:row-span-3 min-h-80">
           <div className="bg-base-100 w-full h-full border-none rounded-lg sm:p-7 p-4 flex flex-col gap-5 transition-transform duration-300 ease-out hover:transform-[rotateX(-4deg)_rotateY(-4deg)_rotateZ(-1deg)_scale(1.02)] hover:cursor-pointer">
             <img
               src="https://avatars.githubusercontent.com/u/107088774?v=4"
               alt="Avatar"
-              className="size-32 2xl:size-64 rounded-full mx-auto my-auto hover:rotate-20 transition-transform duration-800 ease-out"
+              className="size-32 2xl:size-48 rounded-full mx-auto my-auto hover:rotate-20 transition-transform duration-800 ease-out"
             />
             <div className="mt-auto">
               <p className="text-xl font-semibold mb-2">Hi, I'm Angel!</p>
@@ -33,7 +33,7 @@ function App() {
           </div>
         </div>
 
-        <div className="col-span-1 xl:row-span-3 md:min-h-60">
+        <div className="col-span-1 xl:row-span-3 min-h-80">
           <div className="bg-base-100 w-full h-full border-none rounded-lg sm:p-7 p-4 flex flex-col gap-5 transition-transform duration-300 ease-out hover:transform-[rotateX(-4deg)_rotateY(-4deg)_rotateZ(-1deg)_scale(1.02)] hover:cursor-pointer">
             <div className="w-full max-h-fit min-h-0 rounded-lg flex justify-center hover:rotate-10 transition-transform duration-300 ease-out">
               <OrbitImages
@@ -73,7 +73,7 @@ function App() {
         </div>
 
         <div className="xl:col-span-2 xl:row-span-3 perspective-[1000px]">
-          <div className="md:bg-[url('src/assets/game.png')] bg-center bg-base-100 w-full h-full border-none rounded-lg sm:p-7 p-4 flex flex-col gap-5 transition-transform duration-300 ease-out hover:transform-[rotateX(-4deg)_rotateY(-4deg)_rotateZ(-1deg)_scale(1.02)] hover:brightness-105 hover:cursor-pointer">
+          <div className="md:bg-[url('src/assets/game.png')] bg-center bg-base-100 w-full h-full border-none rounded-lg sm:p-7 p-4 flex flex-col gap-5 transition-transform duration-300 ease-out hover:transform-[rotateX(-1deg)_rotateY(-1deg)_rotateZ(-1deg)_scale(1.02)] hover:brightness-105 hover:cursor-pointer">
             <div className="w-full flex-1 min-h-0 overflow-hidden rounded-lg flex justify-center md:hidden">
               <img
                 src="src/assets/work-grid.png"
@@ -118,38 +118,6 @@ function App() {
                       src="https://skill-icons-web.vercel.app/skill-icons/icons/Gmail-Dark.svg"
                       alt="Gmail"
                       className="size-16 2xl:size-14 transition-transform duration-300 ease-out hover:scale-110 hover:rotate-[-10deg]"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="hidden 2xl:flex justify-center">
-              <div className="flex flex-row gap-4">
-                <a href="https://github.com/anqilique" target="_blank">
-                  <img
-                    src="https://skill-icons-web.vercel.app/skill-icons/icons/Github-Dark.svg"
-                    alt="GitHub"
-                    className="size-14 transition-transform duration-300 ease-out hover:scale-110 hover:rotate-[-10deg]"
-                  />
-                </a>
-
-                <a href="https://www.instagram.com/anqilique/" target="_blank">
-                  <img
-                    src="https://skill-icons-web.vercel.app/skill-icons/icons/Instagram.svg"
-                    alt="Instagram"
-                    className="size-14 transition-transform duration-300 ease-out hover:scale-110 hover:rotate-[-10deg]"
-                  />
-                </a>
-                <div
-                  className="lg:tooltip lg:tooltip-bottom tooltip-info"
-                  data-tip="angelbhulot@gmail.com"
-                >
-                  <a href="mailto:angelbhulot@gmail.com">
-                    <img
-                      src="https://skill-icons-web.vercel.app/skill-icons/icons/Gmail-Dark.svg"
-                      alt="Gmail"
-                      className="size-14 transition-transform duration-300 ease-out hover:scale-110 hover:rotate-[-10deg]"
                     />
                   </a>
                 </div>
